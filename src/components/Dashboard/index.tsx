13d01e728e156efb9b08ec01811359bf71a79726
Marco <marcomonteiro94@hotmail.com>
2021-03-25 21:23:39 -0300

    Criado modal

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -3,6 +3,9 @@ import TransactionsTable from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
+  const link =
+    "https://portallac.unimedjf.coop.br:8443/LaboratorioWeb/loginPaciente.action?nmUsuario=329&dsSenha=NEny_2106";
+
   return (
     <Container>
       <Summary />
